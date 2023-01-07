14efba021e5eafa9b4d7b58e25505f2dda730748
Prachaya Orraeiam <prachaya.or@Prachayas-MacBook-Pro.local>
2023-01-07 14:15:53 +0700

    14. Completing the Update Logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -49,8 +49,30 @@ export class AppController {
   }
 
   @Put(':id')
-  updateReport() {
-    return 'update';
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { source: string; amount: number },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+
+    return data.report[reportIndex];
   }
 
   @Delete()
