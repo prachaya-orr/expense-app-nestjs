ceeb9be47feb50b508054127479a7d99e23f707d
Prachaya Orraeiam <prachaya.orr@github.com>
2023-01-08 20:08:05 +0700

    25. Making Properties Optional

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,7 +12,7 @@ import {
 } from '@nestjs/common';
 import { data, ReportType } from './data';
 import { AppService } from './app.service';
-import { CreateReportDto } from './dtos/report.dto';
+import { CreateReportDto, UpdateReportDto } from './dtos/report.dto';
 
 @Controller('report/:type')
 export class AppController {
@@ -52,7 +52,7 @@ export class AppController {
   updateReport(
     @Param('type', new ParseEnumPipe(ReportType)) type: string,
     @Param('id', ParseUUIDPipe) id: string,
-    @Body() body: { source: string; amount: number },
+    @Body() body: UpdateReportDto,
   ) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
